522734530ca9753d238c61d2805eb5f8c63f3cae
Vesek <venda.straka@gmail.com>
2023-10-26 00:34:07 +0200

    Add debug settings and more more config

--- main.py
+++ main.py
@@ -2,34 +2,40 @@ import cv2
 import numpy as np
 import math
 from picamera2 import Picamera2
+import time
 
+headless = True
 running_on_rpi = True
+
+framecounter = 0
+lastsecond = 0
 
 if running_on_rpi: picam2 = Picamera2()
 else: cam = cv2.VideoCapture(0)
 
 if running_on_rpi:
-    config = picam2.create_preview_configuration({"format": 'RGB888', "size": (640, 480)})
+    config = picam2.create_preview_configuration({"format": 'RGB888', "size": (640, 480)}, raw=picam2.sensor_modes[4])
     picam2.configure(config)
     picam2.start()
 
 while True:
+    frametime = time.time()
     if running_on_rpi: frame = picam2.capture_array()
     else: check, frame = cam.read()
-    print(frame.shape)
-
+    # print(frame.shape)
+    gettime = time.time()
     framegray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
-    # ret2,th2 = cv2.threshold(framegray,0,255,cv2.THRESH_OTSU)
+    # ret,th = cv2.threshold(framegray,0,255,cv2.THRESH_OTSU)
 
-    th3 = cv2.adaptiveThreshold(framegray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,33,2)
+    th = cv2.adaptiveThreshold(framegray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,33,2)
 
-    inverted = cv2.bitwise_not(th3)
-
+    inverted = cv2.bitwise_not(th)
+    graythreshold = time.time()
     kernel = np.ones((5,5),np.uint8)
     opening = cv2.morphologyEx(inverted, cv2.MORPH_OPEN, kernel)
 
-    image_canny = cv2.Canny(opening, 1, 3)
+    canny = cv2.Canny(opening, 1, 3)
 
     rho = 1
     theta = np.pi/180
@@ -37,10 +43,10 @@ while True:
     min_line_length = 50
     max_line_gap = 10
     line_image = np.copy(frame)
-
-    lines = cv2.HoughLines(image_canny, rho, theta, threshold, None,
+    morphedge = time.time()
+    lines = cv2.HoughLines(canny, rho, theta, threshold, None,
                             min_line_length, max_line_gap)
-
+    houghtime = time.time()
     if lines is not None:
         render_lines = [line for line in lines if line[0][1] > 2 or line[0][1] < 1]
         for line in render_lines:
@@ -52,14 +58,16 @@ while True:
             y0 = b * rho
             pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
             pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
-            print(theta-2)
+            # print(theta-2)
             cv2.line(line_image, pt1, pt2, (0,0,255), 1, cv2.LINE_AA)
-    cv2.imshow('video', line_image)
+    framecounter += 1
+    if not headless:
+        cv2.imshow('video', line_image)
+        if cv2.waitKey(1) == 27:
+            break
+    print(f"Frame: {framecounter} Total Frametime: {time.time()-frametime} Gettime: {gettime-frametime} Graythreshold: {graythreshold-gettime} Morphedge: {morphedge-graythreshold} Houghtime: {houghtime-morphedge}")
     # cv2.imwrite("sus.png",line_image)
     # break
-    key = cv2.waitKey(1)
-    if key == 27:
-        break
 
 if not running_on_rpi: cam.release()
-cv2.destroyAllWindows()
+if not headless: cv2.destroyAllWindows()
