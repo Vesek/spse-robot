2b7bf4e67213c140dc36c90acd65f01b0479a9c6
Vesek <venda.straka@gmail.com>
2023-10-27 15:37:55 +0200

    Add framebuffer output

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import math
 from picamera2 import Picamera2
 import time
 
-headless = True
+headless = False
+in_fb = True
 running_on_rpi = True
 
 framecounter = 0
@@ -14,7 +15,7 @@ if running_on_rpi: picam2 = Picamera2()
 else: cam = cv2.VideoCapture(0)
 
 if running_on_rpi:
-    config = picam2.create_preview_configuration({"format": 'RGB888', "size": (640, 480)}, raw=picam2.sensor_modes[4])
+    config = picam2.create_preview_configuration({"format": 'RGB888', "size": (640, 480)})
     picam2.configure(config)
     picam2.start()
 
@@ -28,11 +29,11 @@ while True:
 
     # ret,th = cv2.threshold(framegray,0,255,cv2.THRESH_OTSU)
 
-    th = cv2.adaptiveThreshold(framegray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,33,2)
+    th = cv2.adaptiveThreshold(framegray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,33,4)
 
     inverted = cv2.bitwise_not(th)
     graythreshold = time.time()
-    kernel = np.ones((5,5),np.uint8)
+    kernel = np.ones((3,3),np.uint8)
     opening = cv2.morphologyEx(inverted, cv2.MORPH_OPEN, kernel)
 
     canny = cv2.Canny(opening, 1, 3)
@@ -45,10 +46,15 @@ while True:
     line_image = np.copy(frame)
     morphedge = time.time()
     lines = cv2.HoughLines(canny, rho, theta, threshold, None,
-                            min_line_length, max_line_gap)
+                            min_line_length, max_line_gap, -1, 1)
     houghtime = time.time()
+    render_lines = []
+    theta_avg = None
     if lines is not None:
-        render_lines = [line for line in lines if line[0][1] > 2 or line[0][1] < 1]
+        # render_lines = lines
+        render_lines = [line for line in lines if line[0][1] > 0 or line[0][1] < 0]
+        # print(render_lines)
+        theta_avg = 0 if render_lines != [] else None
         for line in render_lines:
             rho = line[0][0]
             theta = line[0][1]
@@ -58,13 +64,23 @@ while True:
             y0 = b * rho
             pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
             pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
+            theta_avg += theta
             # print(theta-2)
             cv2.line(line_image, pt1, pt2, (0,0,255), 1, cv2.LINE_AA)
+    if theta_avg is not None:
+        theta_avg = theta_avg / len(render_lines)
+        print(theta_avg)
     framecounter += 1
     if not headless:
-        cv2.imshow('video', line_image)
-        if cv2.waitKey(1) == 27:
-            break
+        if in_fb:
+            frame32 = cv2.cvtColor(line_image, cv2.COLOR_BGR2BGRA)
+            fbframe = cv2.resize(frame32, (1920,1080))
+            with open('/dev/fb0', 'rb+') as buf:
+                buf.write(fbframe)
+        else:
+            cv2.imshow('video', line_image)
+            if cv2.waitKey(1) == 27:
+                break
     print(f"Frame: {framecounter} Total Frametime: {time.time()-frametime} Gettime: {gettime-frametime} Graythreshold: {graythreshold-gettime} Morphedge: {morphedge-graythreshold} Houghtime: {houghtime-morphedge}")
     # cv2.imwrite("sus.png",line_image)
     # break
