9e24a42484cfc6b2dcc075dd3ed3084c9bad902d
Vesek <venda.straka@gmail.com>
2023-10-29 17:02:25 +0100

    Remove a redundant setting

--- src/picam.py
+++ src/picam.py
@@ -4,7 +4,7 @@ import cv2
 class Camera:
     def __init__(self):
         self.picam2 = Picamera2()
-        config = self.picam2.create_preview_configuration({"format": 'RGB888', "size": (640, 480)})
+        config = self.picam2.create_preview_configuration({"format": 'RGB888'})
         self.picam2.configure(config)
         self.picam2.start()
 
