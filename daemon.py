1467d16173f0d9d578b4bf1750efd7b85cd3798e
Vesek <venda.straka@gmail.com>
2023-11-24 16:26:40 +0100

    Make adjustments to the systemd related files

--- daemon.py
+++ daemon.py
@@ -7,9 +7,15 @@ if __name__ == '__main__':
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(21, GPIO.IN, GPIO.PUD_UP)
     def onButton(channel):
-        print("Button pressed! Starting script")
-        # os.system("bash /home/pi/spse-robot/")
-    GPIO.add_event_detect(21, GPIO.FALLING, callback=onButton, bouncetime=500)
+        print("Button pressed!")
+        if os.system("systemctl --user is-active --quiet spse-robot") != 0:
+            print("Starting spse-robot")
+            os.system("systemctl --user start spse-robot")
+        else:
+            print("Stopping spse-robot")
+            os.system("systemctl --user stop spse-robot")
+        # os.system("")
+    GPIO.add_event_detect(21, GPIO.FALLING, callback=onButton, bouncetime=1000)
     systemd.daemon.notify('READY=1')
     while True:
         time.sleep(1)
