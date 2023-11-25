59a171e2a5cc552496402805734ce92f72cefbdf
Vesek <venda.straka@gmail.com>
2023-11-25 16:00:15 +0100

    Add a audio daemon

--- daemon.py
+++ daemon.py
@@ -8,13 +8,17 @@ if __name__ == '__main__':
     GPIO.setup(21, GPIO.IN, GPIO.PUD_UP)
     def onButton(channel):
         print("Button pressed!")
-        if os.system("systemctl --user is-active --quiet spse-robot") != 0:
+        if os.system("systemctl --user is-active --quiet spse-robot") != 0 or os.system("systemctl --user is-active --quiet spse-robot-audio") != 0:
             print("Starting spse-robot")
             os.system("systemctl --user start spse-robot")
+            print("Starting spse-robot-audio")
+            os.system("systemctl --user start spse-robot-audio")
             # time.sleep(2)
         else:
             print("Stopping spse-robot")
             os.system("systemctl --user stop spse-robot")
+            print("Stopping spse-robot-audio")
+            os.system("systemctl --user stop spse-robot-audio")
             # time.sleep(2)
     GPIO.add_event_detect(21, GPIO.FALLING, callback=onButton, bouncetime=1000)
     systemd.daemon.notify('READY=1')
