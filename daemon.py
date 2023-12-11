a8757bea4c1d1590deca6d56ce4431c168958ec2
Vesek <venda.straka@gmail.com>
2023-12-11 23:08:03 +0100

    Semi-working daemon It can only start the process once but i am already working on a fix. This was a real pain to make because i needed to make ANOTHER camera abstraction.

--- daemon.py
+++ daemon.py
@@ -4,12 +4,17 @@ import time
 import os
 from spse_robot import Robot
 from src.picam import Camera
+from src.threadcamera import ThreadCamera
 from multiprocessing import Process
+from multiprocessing import shared_memory
 import signal
 import sys
+import math
+import numpy as np
+
 
 class namespace:
-    headless=True
+    headless=False
     use_fb=True
     motors=False
     show_raw=False
@@ -20,20 +25,35 @@ class namespace:
     speed=0x6666
     accel=speed/2
     servo=False
+    verbose=True
     running_on_rpi=True
+    fb_size=(1920, 1080)
 
+real_camera = Camera()
+frame = real_camera.capture()
+shm = shared_memory.SharedMemory(create=True, size=frame.nbytes)
+buffer = np.ndarray(frame.shape, dtype=frame.dtype, buffer=shm.buf)
 
-def handler(signal, frame):
-    sys.exit(0)
-
-signal.signal(signal.SIGINT, handler)
-camera = Camera()
+camera = ThreadCamera(shm.name, frame.shape, frame.dtype)
 robot = Robot(camera, namespace)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.IN, GPIO.PUD_UP)
 
 process = Process(target=robot.main_loop)
+
+def signalHandler(signal, frame):
+    try:
+        print("Stopping main loop")
+        process.terminate()
+    except AttributeError:
+        pass
+    print("Stopping spse-robot-audio")
+    os.system("systemctl --user stop spse-robot-audio")
+    process.join()
+    shm.close()
+    shm.unlink()
+    sys.exit(0)
 
 def onButton(channel):
     global process
@@ -45,13 +65,19 @@ def onButton(channel):
         print("Starting spse-robot-audio")
         os.system("systemctl --user start spse-robot-audio")
     else:
-        print("Stopping main loop")
-        process.terminate()
+        try:
+            print("Stopping main loop")
+            process.terminate()
+        except AttributeError:
+            pass
         print("Stopping spse-robot-audio")
         os.system("systemctl --user stop spse-robot-audio")
 
+signal.signal(signal.SIGINT, signalHandler)
 GPIO.add_event_detect(21, GPIO.FALLING, callback=onButton, bouncetime=1000)
 print("Initialzed!")
 systemd.daemon.notify('READY=1')
 while True:
-    time.sleep(1)
+    frame = real_camera.capture()
+    buffer[:] = frame[:]
+    print(buffer.shape)
---
+++ src/threadcamera.py
@@ -0,0 +1,13 @@
+from multiprocessing import shared_memory
+import numpy as np
+
+class ThreadCamera: # WHY DOES THIS HAVE TO EXIST (i want to unalive myself)
+    def __init__(self,mem_name,shape,dtype):
+        self.shm = shared_memory.SharedMemory(name=mem_name)
+        self.buffer = np.ndarray(shape, dtype=dtype, buffer=self.shm.buf)
+
+    def __del__(self):
+        self.shm.close()
+
+    def capture(self):
+        return self.buffer
