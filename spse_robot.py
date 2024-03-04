600e89a19add583cdc588f6f10fef3c749dbfe2e
Vesek <venda.straka@gmail.com>
2024-03-04 19:45:25 +0100

    Actually pretty nice Except for the color

--- spse_robot.py
+++ spse_robot.py
@@ -117,9 +117,9 @@ class Robot:
                     now_time = time.time()
 
                     if self.args.stop_on_line:
-                        sf_detect = analyzer.stop_line_detect(contour, (int(frame.shape[0] * 0.15), int(frame.shape[1] * 0.45)), (int(frame.shape[0] * 0.85), int(frame.shape[1] * 0.45)))  # Completely ✨ arbitrary ✨ numbers
+                        sf_detect = analyzer.stop_line_detect(contour, (int(frame.shape[1] * 0.15), int(frame.shape[0] * 0.45)), (int(frame.shape[1] * 0.85), int(frame.shape[0] * 0.45)))  # Completely ✨ arbitrary ✨ numbers
                         if sf_detect and ((time.time() - start_time) > 10):
-                            cv2.imwrite('amogus.jpg', cv2.circle(img=frame, center=(int(frame.shape[0] * 0.15), int(frame.shape[1] * 0.45)), radius=5, color=(0,0,255), thickness=-1))
+                            # cv2.imwrite('amogus.jpg', cv2.circle(img=frame, center=(int(frame.shape[0] * 0.15), int(frame.shape[1] * 0.45)), radius=5, color=(0,0,255), thickness=-1))
                             stop_time = time.time()
                         if stop_time is not None and ((time.time() - stop_time) > 0.5):
                             break
@@ -159,6 +159,9 @@ class Robot:
                         out_image = preprocessed_frame
                     if self.args.show_raw:
                         out_image = frame
+                    out_image = cv2.circle(out_image, (int(out_image.shape[1] * 0.15), int(out_image.shape[0] * 0.45)), 7, (0,255,0), -1)
+                    out_image = cv2.circle(out_image, (int(out_image.shape[1] * 0.85), int(out_image.shape[0] * 0.45)), 7, (0,255,0), -1)
+                    print((int(out_image.shape[0] * 0.15), int(out_image.shape[1] * 0.45)))
                     if self.args.running_on_rpi and self.args.use_fb:
                         frame32 = cv2.cvtColor(out_image, cv2.COLOR_BGR2BGRA)
                         fbframe = cv2.resize(frame32, self.args.fb_size)
